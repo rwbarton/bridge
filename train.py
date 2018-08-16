9a71ac9a691630f57cec8d20580c14098d12720f
Alya Abbott <alya@mit.edu>
2018-08-15 17:36:33 -0700

    converted training to use keras

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import argparse
 import progressbar
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 import os
 
 HANDS_DIR = "dat"
@@ -82,56 +83,79 @@ del hands
 del bids
 del valid
 
-MIDDLE = 500
-x = tf.placeholder(tf.float32, [None, INPUTS])
-W1 = weight_variable([INPUTS, MIDDLE])
-b1 = bias_variable([MIDDLE])
-x1 = tf.nn.relu(tf.matmul(x, W1) + b1)
-W = weight_variable([MIDDLE,NBIDS])
-b = bias_variable([NBIDS])
-y = tf.matmul(x1, W) + b
-y_ = tf.placeholder(tf.float32, [None, NBIDS])
+def train_tf_model():
+    MIDDLE = 500
+    x = tf.placeholder(tf.float32, [None, INPUTS])
+    W1 = weight_variable([INPUTS, MIDDLE])
+    b1 = bias_variable([MIDDLE])
+    x1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+    W = weight_variable([MIDDLE,NBIDS])
+    b = bias_variable([NBIDS])
+    y = tf.matmul(x1, W) + b
+    y_ = tf.placeholder(tf.float32, [None, NBIDS])
 
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
 
-#train_step = tf.train.GradientDescentOptimizer(0.015).minimize(cross_entropy)
-train_step = tf.train.AdamOptimizer(learn).minimize(cross_entropy)
+    #train_step = tf.train.GradientDescentOptimizer(0.015).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(learn).minimize(cross_entropy)
 
-sess = tf.InteractiveSession()
-tf.global_variables_initializer().run()
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
 
-BATCH_SIZE=20
-training_hands = int(0.8*total_hands)
-for l in range(iters):
-    tot = 0
-    bar = progressbar.ProgressBar()
-    for k in bar(range(training_hands//BATCH_SIZE)):
-        batch_xs = all_in[BATCH_SIZE*k:BATCH_SIZE*(k+1)]
-        batch_ys = all_out[BATCH_SIZE*k:BATCH_SIZE*(k+1)]
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-        # score = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys})
-        # tot += score
-        # if k % 200 == 0:
-        #     print(k * BATCH_SIZE, tot/200)
-        #     tot = 0
+    BATCH_SIZE=20
+    training_hands = int(0.8*total_hands)
+    for l in range(iters):
+        tot = 0
+        bar = progressbar.ProgressBar()
+        for k in bar(range(training_hands//BATCH_SIZE)):
+            batch_xs = all_in[BATCH_SIZE*k:BATCH_SIZE*(k+1)]
+            batch_ys = all_out[BATCH_SIZE*k:BATCH_SIZE*(k+1)]
+            sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+            # score = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys})
+            # tot += score
+            # if k % 200 == 0:
+            #     print(k * BATCH_SIZE, tot/200)
+            #     tot = 0
 
-    # Measure accuracy
-    # Doing this in a single batch uses an unreasonable amount of memory.
-    ACC_BATCH_SIZE=10000
-    next_i = training_hands
-    correct = 0
-    while next_i < total_hands:
-        i = next_i
-        next_i += ACC_BATCH_SIZE
-        correct += sess.run(accuracy, feed_dict={x: all_in[i:next_i],
-                                                 y_: all_out[i:next_i]})
-    acc = correct / (total_hands - training_hands)
-    print(l, "Acc", acc, flush=True)
+        # Measure accuracy
+        # Doing this in a single batch uses an unreasonable amount of memory.
+        ACC_BATCH_SIZE=10000
+        next_i = training_hands
+        correct = 0
+        while next_i < total_hands:
+            i = next_i
+            next_i += ACC_BATCH_SIZE
+            correct += sess.run(accuracy, feed_dict={x: all_in[i:next_i],
+                                                     y_: all_out[i:next_i]})
+        acc = correct / (total_hands - training_hands)
+        print(l, "Acc", acc, flush=True)
 
-saver = tf.train.Saver()
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-save_path = saver.save(sess, os.path.join(OUTPUT_DIR, 'hands%d_%d.ckpt' % (ply, iters)))
-print("Model saved in %s" % save_path)
+    saver = tf.train.Saver()
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    save_path = saver.save(sess, os.path.join(OUTPUT_DIR, 'hands%d_%d.ckpt' % (ply, iters)))
+    print("Model saved in %s" % save_path)
+
+def train_tf_keras_model():
+    model = keras.Sequential()
+    NUNITS = [500, 500]
+    training_hands = int(0.8*total_hands)
+    BATCH_SIZE=20
+
+    for unit_count in NUNITS:
+        model.add(keras.layers.Dense(unit_count, activation='relu'))
+    model.add(keras.layers.Dense(NBIDS, activation='softmax'))
+    model.compile(optimizer=tf.train.AdamOptimizer(learn),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+    model.fit(all_in[:training_hands], all_out[:training_hands], epochs=iters, batch_size=BATCH_SIZE, validation_data = (all_in[training_hands:], all_out[training_hands:]))
+
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    save_path = os.path.join(OUTPUT_DIR, 'hands%d_%d.ckpt' % (ply, iters))
+    model.save_weights(save_path)
+    print("Model saved in %s" % save_path)
+
+train_tf_keras_model()    
